5f9339b3c5ce35c564275baf4b542fa9d1d14421
zhangchuan <zhangchuan2782@ftsafe.com>
2020-09-04 13:32:09 +0800

    Fix a few bugs

--- JuBiterSDKUI/View/JUBCustomInputAlert.m
+++ JuBiterSDKUI/View/JUBCustomInputAlert.m
@@ -152,7 +152,7 @@
     forControlEvents:UIControlEventEditingChanged];
         
     //Wait to set the keyboard type
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [textField becomeFirstResponder];
     });
     
